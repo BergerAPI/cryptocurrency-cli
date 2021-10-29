7bf1b00535eb59f7201b9c18a29ab4c257a4eabb
niclas <bergerbtw@gmail.com>
2021-10-29 14:31:44 +0200

    added simple parameter system, and building with pkg

--- source/main.ts
+++ source/main.ts
@@ -1,1 +1,15 @@
-console.log("Naruto.")
+import Parser from "./parameter";
+
+/**
+ * The main function.
+ */
+function run() {
+	const parameter = new Parser()
+		.param("test", Boolean)
+		.param("test2", Number, true)
+		.parse();
+
+	console.log(parameter.get("test2"));
+}
+
+run();
