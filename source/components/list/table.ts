f93d3b3afaf3b33f55812eddb5bb9e64a4f194b0
niclas <bergerbtw@gmail.com>
2021-10-30 01:32:40 +0200

    cells aren't limited to 4 lines anymore

--- source/components/list/table.ts
+++ source/components/list/table.ts
@@ -1,4 +1,4 @@
-import {Component} from "../component";
+import { Component } from "../component";
 
 /**
  * Maximal length of a table cell.
@@ -10,26 +10,23 @@ export const MAX_LENGTH: number = 30;
  * An entry, that can have multiple lines
  */
 class TableEntry {
-    /**
-     * All lines of this entry
-     */
-    lines: any[][] = [];
+	/**
+	 * All lines of this entry
+	 */
+	lines: any[][] = [];
 
-    constructor(public raw: any[]) {
-        // Checking if an entry is too long
-        const biggest = Math.max(...this.raw.map(element => element.toString().length));
+	constructor(public raw: any[]) {
+		// Checking if an entry is too long
+		const biggest = Math.max(...raw.map(element => element.toString().length));
 
-        // First line.
-        this.lines.push(this.raw.map(x => x.toString().substring(0, MAX_LENGTH)));
+		// First line.
+		this.lines.push(raw.map(x => x.toString().substring(0, MAX_LENGTH)));
 
-        // We have an entry that is too long
-        if (biggest > MAX_LENGTH) {
-
-            // Remaining lines.
-            for (let i = 0; i < this.raw.length; i++)
-                this.lines.push(this.raw.map(x => x.toString().substring(MAX_LENGTH * (i + 1), MAX_LENGTH * (i + 2))));
-        }
-    }
+		// Remaining lines.
+		if (biggest > MAX_LENGTH)
+			for (let i = 1; i < Math.ceil((biggest / MAX_LENGTH) / 1) * 1; i++)
+				this.lines.push(raw.map(x => x.toString().substring(MAX_LENGTH * i, MAX_LENGTH * (i + 1)).trim()));
+	}
 }
 
 /**
@@ -37,82 +34,81 @@ class TableEntry {
  */
 export class Table implements Component {
 
-    /**
-     * We need to border the table.
-     */
-    borders: string[] = ["┌", "└", "┐", "┘", "─", "│", "┬", "┴", "┼", "├", "┤"];
+	/**
+	 * We need to border the table.
+	 */
+	borders: string[] = ["┌", "└", "┐", "┘", "─", "│", "┬", "┴", "┼", "├", "┤"];
 
-    /**
-     * The table entries.
-     */
-    data: TableEntry[] = [];
+	/**
+	 * The table entries.
+	 */
+	data: TableEntry[] = [];
 
-    constructor(data: any[][] | TableEntry[], public padding: number = 2) {
-        if (this.padding % 2 != 0)
-            throw new Error("Padding must be an even number.");
+	constructor(data: any[][] | TableEntry[], public padding: number = 2) {
+		if (this.padding % 2 != 0)
+			throw new Error("Padding must be an even number.");
 
-        // Mapping to TableEntries if we don't have them already inside the array.
-        if (data[0] instanceof TableEntry)
-            this.data = data as TableEntry[];
-        else
-            this.data = data.map(row => new TableEntry(row as any[]));
-    }
+		// Mapping to TableEntries if we don't have them already inside the array.
+		if (data[0] instanceof TableEntry)
+			this.data = data as TableEntry[];
+		else
+			this.data = data.map(row => new TableEntry(row as any[]));
+	}
 
-    /**
-     * @see Component.print
-     */
-    print(): void {
-        // @ts-ignore
-        const biggestElement = Math.max(...this.data.map(entry => Math.max(...entry.lines.map(row => Math.max(...row.map(element => element.toString().length))))));
-        const longestRow = Math.max(...this.data.map(x => x.raw.length))
-        const width = longestRow * biggestElement + this.padding * longestRow + 2;
+	/**
+	 * @see Component.print
+	 */
+	print(): void {
+		// @ts-ignore
+		const longestRow = Math.max(...this.data.map(x => x.raw.length))
+		const width = longestRow * (MAX_LENGTH + this.padding) + this.padding;
 
-        // A list of all lines
-        let result = [];
+		// A list of all lines
+		let result = [];
 
-        // Printing the table with borders.
-        let line: string = "";
+		// Printing the table with borders.
+		let line: string = "";
 
-        // For setting the connections
-        let counter: number = 0;
-        let steps: number = 0;
+		// For setting the connections
+		let counter: number = 0;
+		let steps: number = 0;
 
-        for (let i = 0; i < width; i++) {
+		for (let i = 0; i < width; i++) {
 
-            // Checking if we'd need to place a straight icon.
-            if (counter == biggestElement + this.padding + steps) {
-                counter = 0;
-                steps++;
-                line += this.borders[6];
-            } else line += this.borders[4];
+			// Checking if we'd need to place a straight icon.
+			if (counter == MAX_LENGTH + this.padding + steps) {
+				counter = 0;
+				steps++;
+				line += this.borders[6];
+			} else line += this.borders[4];
 
-            counter++;
-        }
+			counter++;
+		}
 
-        result.push(this.borders[0] + line + this.borders[2]);
+		result.push(this.borders[0] + line + this.borders[2]);
 
-        this.data.forEach((entries, index) => {
-            entries.lines.forEach(row => {
-                let line = this.borders[5];
+		this.data.forEach((entries, index) => {
+			entries.lines.forEach(row => {
+				let line = this.borders[5];
 
-                row.forEach(cell => {
-                    line += " ".repeat(this.padding / 2)
-                    line += cell.toString()
-                    line += " ".repeat(biggestElement - cell.toString().length + this.padding / 2)
-                    line += this.borders[5]
-                })
+				row.forEach(cell => {
+					line += " ".repeat(this.padding / 2)
+					line += cell.toString()
+					line += " ".repeat(MAX_LENGTH - cell.toString().length + this.padding / 2)
+					line += this.borders[5]
+				})
 
-                result.push(line);
-            });
+				result.push(line);
+			});
 
-            if (index != this.data.length - 1)
-                result.push(this.borders[9] + line.replace(new RegExp(this.borders[6], 'g'), this.borders[8]) + this.borders[10]);
-        });
+			if (index != this.data.length - 1)
+				result.push(this.borders[9] + line.replace(new RegExp(this.borders[6], 'g'), this.borders[8]) + this.borders[10]);
+		});
 
-        // Using a RegExp instead of replaceAll, since I can't use es2021 right now.
-        result.push(this.borders[1] + line.replace(new RegExp(this.borders[6], 'g'), this.borders[7]) + this.borders[3]);
+		// Using a RegExp instead of replaceAll, since I can't use es2021 right now.
+		result.push(this.borders[1] + line.replace(new RegExp(this.borders[6], 'g'), this.borders[7]) + this.borders[3]);
 
-        // Printing the result.
-        result.forEach(line => console.log(line));
-    }
+		// Printing the result.
+		result.forEach(line => console.log(line));
+	}
 }
--- source/main.ts
+++ source/main.ts
@@ -7,11 +7,8 @@ import { Table } from "./components/list/table";
 function run() {
 	const table: Component = new Table([
 		["Name", "Description", "Skills"],
-		["A", "duis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risus", "duis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risus"],
-		["duis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risus", "SIMEX", "duis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risus"],
-		["Niclas", "NARUTO", "duis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risus"],
-		["Dustin", "TTURNA", "duis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risus"],
-		["duis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risus", "MENE", "duis ultricies lacus sed turpis tincidunt id aliquet risusduis ultricies lacus sed turpis tincidunt id aliquet risus"],
+		["John", "John is a nice guy", "JavaScript"],
+		["Mark", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi sit amet mauris commodo quis imperdiet massa. Nibh nisl condimentum id venenatis a condimentum. Viverra adipiscing at in tellus integer feugiat scelerisque. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Maecenas sed enim ut sem viverra aliquet. Dictumst quisque sagittis purus sit amet volutpat consequat. Dictum sit amet justo donec enim diam. Tortor at risus viverra adipiscing at in tellus. Eu feugiat pretium nibh ipsum consequat. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis. A diam sollicitudin tempor id eu nisl. Tincidunt vitae semper quis lectus nulla at volutpat diam. Magna fringilla urna porttitor rhoncus dolor purus non.", "Everything."]
 	]);
 
 	table.print();
