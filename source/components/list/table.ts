ba6587430e2408f65f8cc9842b1e6f686ce961ef
6AM <bergerbtw@gmail.com>
2021-10-30 21:11:22 +0200

    Added basic input.

--- source/components/list/table.ts
+++ source/components/list/table.ts
@@ -1,5 +1,6 @@
 import Colors from "../../color";
-import { Component } from "../component";
+import readline from "readline"
+import {Component} from "../component";
 
 /**
  * Maximal length of a table cell.
@@ -11,44 +12,67 @@ export const MAX_LENGTH: number = 30;
  * An entry, that can have multiple lines
  */
 class TableEntry {
-	/**
-	 * All lines of this entry
-	 */
-	lines: any[][] = [];
+    /**
+     * All lines of this entry
+     */
+    lines: any[][] = [];
 
-	constructor(public raw: any[]) {
-		const expr = new RegExp('(^.{1,' + MAX_LENGTH + '}(\\s+|$))|(^.{1,' + String(MAX_LENGTH - 1) + '}(\\\\|/|_|\\.|,|;|-))');
-		let newLines: any[][] = [];
+    constructor(public raw: any[]) {
+        const expr = new RegExp('(^.{1,' + MAX_LENGTH + '}(\\s+|$))|(^.{1,' + String(MAX_LENGTH - 1) + '}(\\\\|/|_|\\.|,|;|-))');
+        let newLines: any[][] = [];
 
-		raw.forEach((subject, index) => {
-			let currentIndex = 0;
+        raw.forEach((subject, index) => {
+            let currentIndex = 0;
 
-			do {
-				const match = expr.exec(subject);
+            do {
+                const match = expr.exec(subject);
 
-				if (newLines[currentIndex] == undefined) {
-					const newLine: any[] = [];
+                if (newLines[currentIndex] == undefined) {
+                    const newLine: any[] = [];
 
-					for (let index = 0; index < raw.length; index++)
-						newLine.push('');
+                    for (let index = 0; index < raw.length; index++)
+                        newLine.push('');
 
-					newLines.push(newLine);
-				}
+                    newLines.push(newLine);
+                }
 
-				if (match) {
-					subject = subject.slice(match[0].length);
-					newLines[currentIndex][index] = match[0];
-				} else {
-					subject = subject.slice(MAX_LENGTH);
-					newLines[currentIndex][index] = subject.slice(0, MAX_LENGTH);
-				}
+                if (match) {
+                    subject = subject.slice(match[0].length);
+                    newLines[currentIndex][index] = match[0];
+                } else {
+                    subject = subject.slice(MAX_LENGTH);
+                    newLines[currentIndex][index] = subject.slice(0, MAX_LENGTH);
+                }
 
-				currentIndex++;
-			} while (subject.length);
-		});
+                currentIndex++;
+            } while (subject.length);
+        });
 
-		newLines.forEach(line => this.lines.push(line));
-	}
+        newLines.forEach(line => this.lines.push(line));
+    }
+}
+
+/**
+ * A config for our table.
+ */
+export class TableConfig {
+    /**
+     * How much space we want in each row.
+     * Note: This must be a multiple of 2.
+     */
+    padding: number = 2;
+
+    /**
+     * If the table should be scrollable.
+     * Note: If true, this will stop the entire program,
+     *       until we got a result.
+     */
+    scroll: boolean = false;
+
+    /**
+     * If we have an header.
+     */
+    header: boolean = true;
 }
 
 /**
@@ -56,69 +80,132 @@ class TableEntry {
  */
 export class Table implements Component {
 
-	/**
-	 * We need to border the table.
-	 */
-	borders: string[] = ["┌", "└", "┐", "┘", "─", "│", "┬", "┴", "┼", "├", "┤"];
+    /**
+     * If we need keyboard input for user interaction.
+     */
+    requireKeyInput: boolean = true;
 
-	/**
-	 * The table entries.
-	 */
-	data: TableEntry[] = [];
+    /**
+     * We need to border the table.
+     * @private
+     */
+    private borders: string[] = ["┌", "└", "┐", "┘", "─", "│", "┬", "┴", "┼", "├", "┤"];
 
-	constructor(data: any[][] | TableEntry[], public padding: number = 2) {
-		if (this.padding % 2 != 0)
-			throw new Error("Padding must be an even number.");
+    /**
+     * The table entries.
+     * @private
+     */
+    private readonly data: TableEntry[] = [];
 
-		// Mapping to TableEntries if we don't have them already inside the array.
-		if (data[0] instanceof TableEntry)
-			this.data = data as TableEntry[];
-		else
-			this.data = data.map(row => new TableEntry(row as any[]));
-	}
+    /**
+     * We need this for scrolling through the table.
+     * @private
+     */
+    private currentRow: number = 0;
 
-	/**
-	 * @see Component.print
-	 */
-	print(): void {
-		// The biggest element
-		const biggest = Math.max(...this.data.map(x => Math.max(...x.lines.map(y => Math.max(...y.map(z => z.length))))));
-		const longestRow = Math.max(...this.data.map(x => x.raw.length))
+    constructor(data: any[][] | TableEntry[], public config: TableConfig = new TableConfig(), public callback: (entry: TableEntry) => void = () => {}) {
+        if (this.config.padding % 2 != 0)
+            throw new Error("Padding must be an even number.");
 
-		// A list of all lines
-		let result = [];
+        // If we don't want a scrollable table, we can just set
+        // the `requireKeyInput` to false
+        if (this.config.scroll) {
+            // Preparing for input.
+            readline.emitKeypressEvents(process.stdin);
+            process.stdin.setRawMode(true);
 
-		// Splitting here to a string array, since we need to change some characters.
-		let joinLine: string[] = this.borders[4].repeat((this.padding + biggest) * longestRow + (longestRow - 1)).split("");
+            process.stdin.on('keypress', (str, key) => {
+                this.handleKeyInput(key.name)
+            })
 
-		this.data.forEach((entries, index) => {
-			entries.lines.forEach(row => {
-				let line = this.borders[5];
+            if (this.config.header)
+                this.currentRow++;
+        }
 
-				row.forEach((rawCell, cellIndex) => {
-					rawCell += Colors.reset;
+        // Mapping to TableEntries if we don't have them already inside the array.
+        if (data[0] instanceof TableEntry)
+            this.data = data as TableEntry[];
+        else
+            this.data = data.map(row => new TableEntry(row as any[]));
+    }
 
-					// We need the cell without any color codes.
-					let cell = rawCell.toString().replace(/\u001b\[\d+m/g, "");
+    /**
+     * @see Component.print
+     */
+    print(): void {
+        // The biggest element
+        const biggest = Math.max(...this.data.map(x => Math.max(...x.lines.map(y => Math.max(...y.map(z => z.length))))));
+        const longestRow = Math.max(...this.data.map(x => x.raw.length))
 
-					if (cellIndex + 1 < row.length)
-						joinLine[line.replace(/\u001b\[\d+m/g, "").length + this.padding / 2 + biggest] = this.borders[6];
+        // A list of all lines
+        let result = [];
 
-					line += " ".repeat(this.padding / 2) + rawCell + " ".repeat(biggest - cell.length + this.padding / 2) + this.borders[5]
-				})
+        // Splitting here to a string array, since we need to change some characters.
+        let joinLine: string[] = this.borders[4].repeat((this.config.padding + biggest) * longestRow + (longestRow - 1)).split("");
 
-				result.push(line);
-			});
+        this.data.forEach((entries, index) => {
+            entries.lines.forEach((row) => {
+                let line = this.borders[5];
 
-			if (index != this.data.length - 1)
-				result.push(this.borders[9] + joinLine.join("").replace(new RegExp(this.borders[6], 'g'), this.borders[8]) + this.borders[10]);
-		});
+                row.forEach((rawCell, cellIndex) => {
+                    if (this.currentRow == index && this.config.scroll)
+                        rawCell = Colors.whiteBG + Colors.black + rawCell;
 
-		// Using a RegExp instead of replaceAll, since I can't use es2021 right now.
-		result.unshift(this.borders[0] + joinLine.join("") + this.borders[2]);
-		result.push(this.borders[1] + joinLine.join("").replace(new RegExp(this.borders[6], 'g'), this.borders[7]) + this.borders[3]);
+                    rawCell += Colors.reset;
 
-		// Printing the result.
-		result.forEach(line => console.log(line));
-	}
+                    // We need the cell without any color codes.
+                    let cell = rawCell.toString().replace(/\u001b\[\d+m/g, "");
+
+                    if (cellIndex + 1 < row.length)
+                        joinLine[line.replace(/\u001b\[\d+m/g, "").length + this.config.padding / 2 + biggest] = this.borders[6];
+
+                    line += " ".repeat(this.config.padding / 2) + rawCell + " ".repeat(biggest - cell.length + this.config.padding / 2) + this.borders[5]
+                })
+
+                result.push(line);
+            });
+
+            if (index != this.data.length - 1)
+                result.push(this.borders[9] + joinLine.join("").replace(new RegExp(this.borders[6], 'g'), this.borders[8]) + this.borders[10]);
+        });
+
+        // Using a RegExp instead of replaceAll, since I can't use es2021 right now.
+        result.unshift(this.borders[0] + joinLine.join("") + this.borders[2]);
+        result.push(this.borders[1] + joinLine.join("").replace(new RegExp(this.borders[6], 'g'), this.borders[7]) + this.borders[3]);
+
+        // Printing the result.
+        result.forEach(line => console.log(line));
+    }
+
+    /**
+     * @see Component.handleKey
+     */
+    handleKeyInput(key: string): void {
+        switch (key) {
+            case "q":
+                process.exit();
+                break;
+            case "down":
+                if (this.currentRow < this.data.length - 1)
+                    this.currentRow++;
+
+                break;
+            case "up":
+                if (this.currentRow - 1 != 0)
+                    this.currentRow--;
+
+                break;
+            case "return":
+                process.stdin.destroy();
+                this.callback(this.data[this.currentRow]);
+
+                break;
+        }
+
+        if (key == "down" || key == "up") {
+            // Clearing and Redrawing.
+            process.stdout.write("\x1B[2J\x1B[0f");
+            this.print();
+        }
+    }
 }
--- source/main.ts
+++ source/main.ts
@@ -1,7 +1,6 @@
-import { GeckoApi } from "./api/geckoApi";
+import {GeckoApi} from "./api/geckoApi";
 import Colors from "./color";
-import { Component } from "./components/component";
-import { Table } from "./components/list/table";
+import {Table} from "./components/list/table";
 import Parser from "./parameter";
 
 /**
@@ -9,54 +8,72 @@ import Parser from "./parameter";
  */
 const gecko = new GeckoApi();
 
+/**
+ * All currencies we support with their corresponding symbol.
+ */
 const currencies = {
-	"eur": "€",
-	"usd": "$",
-	"gbp": "£",
-	"cad": "C$",
-	"aud": "A$",
-	"brl": "R$",
-	"cny": "¥",
-	"dkk": "kr",
+    "eur": "€",
+    "usd": "$",
+    "gbp": "£",
+    "cad": "C$",
+    "aud": "A$",
+    "brl": "R$",
+    "cny": "¥",
+    "dkk": "kr",
 }
 
 /**
  * Displaying a table of the top 10 coins.
  * @param currency The currency we show the data in.
+ * @param precision How accurate we want the data to be.
  */
-async function displayTable(currency: string, percision: number) {
-	const coins = await gecko.coin.markets(currency);
+async function displayTable(currency: string, precision: number) {
+    const coins = await gecko.coin.markets(currency);
 
-	const table: Component = new Table([
-		[Colors.underline + "Id", Colors.underline + "Price", Colors.underline + "24h High", Colors.underline + "24h Low", Colors.underline + "24h Change"],
-		...coins.map((coin) => {
-			const currencyChar = currencies[currency as keyof typeof currencies] || currency.toUpperCase();
+    const table = new Table([
+        [Colors.underline + "Id", Colors.underline + "Price", Colors.underline + "24h High", Colors.underline + "24h Low", Colors.underline + "24h Change"],
+        ...coins.map((coin) => {
+            const currencyChar = currencies[currency as keyof typeof currencies] || currency.toUpperCase();
 
-			const price = parseFloat(coin["current_price"]).toFixed(percision).toString() + currencyChar;
-			const high = parseFloat(coin["high_24h"]).toFixed(percision).toString() + currencyChar;
-			const low = parseFloat(coin["low_24h"]).toFixed(percision).toString() + currencyChar;
-			const change = parseFloat(coin["price_change_24h"]).toFixed(percision).toString() + currencyChar;
+            const price = parseFloat(coin["current_price"]).toFixed(precision).toString() + currencyChar;
+            const high = parseFloat(coin["high_24h"]).toFixed(precision).toString() + currencyChar;
+            const low = parseFloat(coin["low_24h"]).toFixed(precision).toString() + currencyChar;
+            const change = parseFloat(coin["price_change_24h"]).toFixed(precision).toString() + currencyChar;
 
-			return [coin["name"], price, high, low, change];
-		}),
-	]);
+            return [coin["name"], price, high, low, change];
+        }),
+    ], {
+        padding: 2,
+        header: true,
+        scroll: true,
+    }, (entry => {
+        // Clearing the screen.
+        process.stdout.write("\x1B[2J\x1B[0f");
 
-	table.print();
+        console.log("Your choice: " + entry.lines[0][0]);
+    }));
+
+    table.print();
 }
 
 /**
  * The main function.
  */
 async function run() {
-	const parameter = new Parser()
-		.param("currency", String)
-		.param("percision", Number)
-		.parse();
+    const parameter = new Parser()
+        .param("currency", String)
+        .param("precision", Number)
+        .parse();
 
-	await displayTable(
-		parameter.get("currency") == false ? "usd" : parameter.get("currency"),
-		parameter.get("percision") == false ? 2 : parameter.get("percision")
-	);
+    // Clearing the screen.
+    process.stdout.write("\x1B[2J\x1B[0f");
+
+    await displayTable(
+        parameter.get("currency") == false ? "usd" : parameter.get("currency"),
+        parameter.get("precision") == false ? 2 : parameter.get("precision")
+    );
 }
 
-run();
+run().then(_ => {
+    /* Done */
+});
